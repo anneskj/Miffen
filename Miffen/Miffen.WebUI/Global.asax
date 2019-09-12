35d1bcec12297dfb4b35033995f808f3503c8570
Anne Lise Skjaveland <annels16@student.uia.no>
2019-09-12 22:05:42 +0200

    Initial Solution Setup

---
+++ Miffen/Miffen.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Miffen.WebUI.MvcApplication" Language="C#" %>
---
+++ Miffen/Miffen.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Miffen.WebUI.Startup))]
+namespace Miffen.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
